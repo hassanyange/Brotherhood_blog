68907ebfc2e827bd82cc69285e4d7061723accb1
hassanyange <hassanyange@gmail.com>
2022-04-26 20:29:51 +0300

    register form

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,16 @@
 from django import http
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 # Create your views here.
+
+
 def home(request):
-    return render (request,'blog/home.html')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+
 def about(request):
-    return render (request,'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
